0b03210956fa67ee5f57e0888235c2ad2a15b15d
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 16:59:36 +0300

    edit

--- src/main/java/by/itacademy/bakumenko/page/BlushPageXpath.java
+++ src/main/java/by/itacademy/bakumenko/page/BlushPageXpath.java
@@ -2,9 +2,9 @@ package by.itacademy.bakumenko.page;
 
 public class BlushPageXpath {
     public static final String LOGIN_INPUT_FEILD = "//*[@id=\"__next\"]/div[1]/div[3]/a/span";
-    public static final String LOGIN_BUTTON_MAIN = "//*[@id=\"__next\"]/div[3]/div/div[1]/form/button/span";
-    public static final String LOGIN_INPUT_PASSWORD = "//input[@id='email']";
-    public static final String LOGIN_INPUT_EMAIL = "//input[@id='password']";
+    public static final String LOGIN_BUTTON_MAIN = "//*[@id=\"__next\"]/div[3]/div/div[1]/form/button";
+    public static final String LOGIN_INPUT_PASSWORD = "//input[@id='password']";
+    public static final String LOGIN_INPUT_EMAIL = "//input[@id='email']";
     public static final String ERROR_MESSAGE_NON_PASSWORD = "//*[@id=\"password-error\"]";
     public static final String ERROR_MESSAGE_NON_EMAIL = "//*[@id=\"email-error\"]";
 }
