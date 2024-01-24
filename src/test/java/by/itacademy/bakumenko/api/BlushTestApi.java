a501a8bcc8e52557b2640aa15d7495bf691cac43
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 13:14:28 +0300

    add new test

--- src/test/java/by/itacademy/bakumenko/api/BlushTestApi.java
+++ src/test/java/by/itacademy/bakumenko/api/BlushTestApi.java
@@ -19,7 +19,7 @@ public class BlushTestApi {
                 post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDUR6XzgDyhoU9Ad4fw6_aoSTXEmVjuhtE").
                 then().
                 statusCode(400).
-                body("error.errors[0].message",equalTo("INVALID_EMAIL"));
+                body("error.errors[0].message", equalTo("INVALID_EMAIL"));
     }
 
     @Test
@@ -67,4 +67,18 @@ public class BlushTestApi {
                 log().all();
     }
 
+    @Test
+    public void testBushLoginNoPassword3() {
+        String body = "{\"email\":\"///\"," +
+                "\"password\":\"1234test\"," +
+                "\"returnSecureToken\":true" +
+                "}";
+        given().
+                body(body).
+                header("Content-Type", "application/json").
+                when().
+                post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDUR6XzgDyhoU9Ad4fw6_aoSTXEmVjuhtE").
+                then().
+                log().all();
+    }
 }
