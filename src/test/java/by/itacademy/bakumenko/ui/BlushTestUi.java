9d2382c99d796e9946d4f406fb2e975fc9082c5b
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 17:05:27 +0300

    edit testLoginCheck()

--- src/test/java/by/itacademy/bakumenko/ui/BlushTestUi.java
+++ src/test/java/by/itacademy/bakumenko/ui/BlushTestUi.java
@@ -1,18 +1,22 @@
 package by.itacademy.bakumenko.ui;
 
+import by.itacademy.bakumenko.domain.User;
 import by.itacademy.bakumenko.page.BlushPage;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class BlushTestUi {
+public class BlushTestUi extends BaseTest {
     @Test
     public void testLoginCheck() throws InterruptedException {
         BlushPage blushPage = new BlushPage();
         blushPage.openMainPage();
+        blushPage.clickLoginInputField();
+        blushPage.sendKeysLoginInputEmail(User.getRandomNonCorrectEmail());
+        blushPage.sendKeysInputPassword(User.getRandomPassword());
         blushPage.clickLoginButtonMain();
-        blushPage.getErrorMessageNonPassword();
         blushPage.getErrorMessageNonEmail();
-        Assertions.assertEquals("Email is required.", blushPage.getErrorMessageNonEmail());
+        Thread.sleep(1000);
+        Assertions.assertEquals("There is no user record corresponding to this identifier. The user may have been deleted.",blushPage.getErrorMessageNonEmail());
     }
 }
