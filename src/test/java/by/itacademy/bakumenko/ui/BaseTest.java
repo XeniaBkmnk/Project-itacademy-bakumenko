7e3ce115865d980111bc2317f97980fa262eccdc
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 15:37:27 +0300

    add method baseFinish()

--- src/test/java/by/itacademy/bakumenko/ui/BaseTest.java
+++ src/test/java/by/itacademy/bakumenko/ui/BaseTest.java
@@ -1,4 +1,15 @@
 package by.itacademy.bakumenko.ui;
 
+import by.itacademy.bakumenko.driver.MyDriver;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.AfterEach;
+
 public class BaseTest {
+    @AfterEach
+    public void baseFinish() throws InterruptedException {
+        Thread.sleep(2000);
+        if (MyDriver.getDriver() != null) {
+            MyDriver.quitDriver();
+        }
+    }
 }
