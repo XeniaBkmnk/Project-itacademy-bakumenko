da320bd8ee143820d53e6f54fef5e6ea45ae2e06
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 14:30:24 +0300

    add new method

--- src/main/java/by/itacademy/bakumenko/driver/MyDriver.java
+++ src/main/java/by/itacademy/bakumenko/driver/MyDriver.java
@@ -1,4 +1,11 @@
 package by.itacademy.bakumenko.driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyDriver {
+    private static WebDriver driver;
+
+    private MyDriver() {
+    }
 }
