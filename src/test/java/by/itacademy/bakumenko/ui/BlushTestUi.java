99ebb48191d4adc6cd33ff2b4301127e5481db2f
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 18:22:01 +0300

    add new testLoginNonCorrectEmail()

--- src/test/java/by/itacademy/bakumenko/ui/BlushTestUi.java
+++ src/test/java/by/itacademy/bakumenko/ui/BlushTestUi.java
@@ -7,6 +7,30 @@ import org.junit.jupiter.api.Test;
 
 public class BlushTestUi extends BaseTest {
     @Test
+    public void testLoginNonUserEmail() throws InterruptedException {
+        BlushPage blushPage = new BlushPage();
+        blushPage.openMainPage();
+        blushPage.clickLoginInputField();
+        blushPage.sendKeysLoginInputEmail(User.getRandomCorrectEmail());
+        blushPage.sendKeysInputPassword(User.getRandomPassword());
+        blushPage.clickLoginButtonMain();
+        blushPage.getErrorMessageNonUserEmail();
+        Assertions.assertEquals("There is no user record corresponding to this identifier. The user may have been deleted.", blushPage.getErrorMessageNonUserEmail());
+    }
+
+    @Test
+    public void testLoginNonPassword() throws InterruptedException {
+        BlushPage blushPage = new BlushPage();
+        blushPage.openMainPage();
+        blushPage.clickLoginInputField();
+        blushPage.sendKeysLoginInputEmail(User.getRandomCorrectEmail());
+        blushPage.sendKeysInputPassword("");
+        blushPage.clickLoginButtonMain();
+        blushPage.getErrorMessageNonPassword();
+        Assertions.assertEquals("Password is required.", blushPage.getErrorMessageNonPassword());
+    }
+
+    @Test
     public void testLoginNonCorrectEmail() throws InterruptedException {
         BlushPage blushPage = new BlushPage();
         blushPage.openMainPage();
@@ -14,8 +38,8 @@ public class BlushTestUi extends BaseTest {
         blushPage.sendKeysLoginInputEmail(User.getRandomNonCorrectEmail());
         blushPage.sendKeysInputPassword(User.getRandomPassword());
         blushPage.clickLoginButtonMain();
-        blushPage.getErrorMessageNonEmail();
-        Thread.sleep(1000);
-        Assertions.assertEquals("There is no user record corresponding to this identifier. The user may have been deleted.",blushPage.getErrorMessageNonEmail());
+        blushPage.getErrorMessageNonCorrectEmail();
+        Assertions.assertEquals("The email address is badly formatted.", blushPage.getErrorMessageNonCorrectEmail());
     }
+
 }
