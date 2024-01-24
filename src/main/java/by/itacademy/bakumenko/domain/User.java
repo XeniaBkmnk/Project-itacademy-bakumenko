8713584425cd89deda29cb5ab91adac34ec64973
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 15:50:44 +0300

    add method getRandomNonCorrectEmail()

--- src/main/java/by/itacademy/bakumenko/domain/User.java
+++ src/main/java/by/itacademy/bakumenko/domain/User.java
@@ -1,4 +1,11 @@
 package by.itacademy.bakumenko.domain;
 
+import com.github.javafaker.Faker;
+
 public class User {
+    public static String getRandomNonCorrectEmail() {
+        Faker faker = new Faker();
+        return faker.name().firstName()
+                + "@test";
+    }
 }
