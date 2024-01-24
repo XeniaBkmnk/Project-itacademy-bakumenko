bee259e09be58dd5fda3ac4e8f5896bc76c09176
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 14:42:06 +0300

    add constructor BlushPage() and method openMainPage()

--- src/main/java/by/itacademy/bakumenko/page/BlushPage.java
+++ src/main/java/by/itacademy/bakumenko/page/BlushPage.java
@@ -1,4 +1,16 @@
 package by.itacademy.bakumenko.page;
 
+import by.itacademy.bakumenko.driver.MyDriver;
+import org.openqa.selenium.WebDriver;
+
 public class BlushPage {
+    private WebDriver driver;
+
+    public BlushPage() {
+        this.driver = MyDriver.getDriver();
+    }
+
+    public void openMainPage() {
+        driver.get("https://blush.design/");
+    }
 }
