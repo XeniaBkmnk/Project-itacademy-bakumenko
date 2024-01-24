27717dff55994c3cf1035b60b08ec23fe7fa8c0c
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 14:53:50 +0300

    add method clickLoginInputPassword()

--- src/main/java/by/itacademy/bakumenko/page/BlushPage.java
+++ src/main/java/by/itacademy/bakumenko/page/BlushPage.java
@@ -22,7 +22,13 @@ public class BlushPage {
     public void clickLoginButtonMain() {
         driver.findElement(By.xpath(BlushPageXpath.LOGIN_BUTTON_MAIN)).click();
     }
-    public void clickLoginInputEmail(){
+
+    public void clickLoginInputEmail() {
         driver.findElement(By.xpath(BlushPageXpath.LOGIN_INPUT_EMAIL)).click();
     }
+
+    public void clickLoginInputPassword() {
+        driver.findElement(By.xpath(BlushPageXpath.LOGIN_INPUT_EMAIL)).click();
+    }
+
 }
