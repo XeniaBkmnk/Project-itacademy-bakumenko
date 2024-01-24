587b119968ac7a05f4764e35b4201461dc6cfa74
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 17:05:38 +0300

    edit

--- src/main/java/by/itacademy/bakumenko/page/BlushPageXpath.java
+++ src/main/java/by/itacademy/bakumenko/page/BlushPageXpath.java
@@ -6,5 +6,5 @@ public class BlushPageXpath {
     public static final String LOGIN_INPUT_PASSWORD = "//input[@id='password']";
     public static final String LOGIN_INPUT_EMAIL = "//input[@id='email']";
     public static final String ERROR_MESSAGE_NON_PASSWORD = "//*[@id=\"password-error\"]";
-    public static final String ERROR_MESSAGE_NON_EMAIL = "//*[@id=\"email-error\"]";
+    public static final String ERROR_MESSAGE_NON_EMAIL = "//*[@id=\"__next\"]/div[3]/div/div[1]/form/div";
 }
