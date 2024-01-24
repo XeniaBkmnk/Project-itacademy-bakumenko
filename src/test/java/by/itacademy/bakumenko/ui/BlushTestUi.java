792d455b6bc1263ec318a3fc4cb79f33c7892e35
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-24 15:34:35 +0300

    add testLoginCheck()

--- src/test/java/by/itacademy/bakumenko/ui/BlushTestUi.java
+++ src/test/java/by/itacademy/bakumenko/ui/BlushTestUi.java
@@ -1,3 +1,18 @@
 package by.itacademy.bakumenko.ui;
+
+import by.itacademy.bakumenko.page.BlushPage;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BlushTestUi {
+    @Test
+    public void testLoginCheck() throws InterruptedException {
+        BlushPage blushPage = new BlushPage();
+        blushPage.openMainPage();
+        blushPage.clickLoginButtonMain();
+        blushPage.getErrorMessageNonPassword();
+        blushPage.getErrorMessageNonEmail();
+        Assertions.assertEquals("Email is required.", blushPage.getErrorMessageNonEmail());
+    }
 }
