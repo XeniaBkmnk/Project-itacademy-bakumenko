cb741335fb3bf9947f260b4f6328579c3354b226
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-26 00:03:01 +0300

    add bodies

--- src/main/java/by/itacademy/bakumenko/page/ApiPage.java
+++ src/main/java/by/itacademy/bakumenko/page/ApiPage.java
@@ -1,5 +1,26 @@
 package by.itacademy.bakumenko.page;
 
 public class ApiPage {
+    public String bodyLoginNoData ="{" +
+            "\"email\":\"\"," +
+            "\"password\":\" \"," +
+            "\"returnSecureToken\":true" +
+            "}";
+    public String bodyLoginNoPassword = "{" +
+            "\"email\":\"test12@gmail.com\"," +
+            "\"password\":\"\"," +
+            "\"returnSecureToken\":true" +
+            "}";
+    public String bodyLoginNoEmail = "{" +
+            "\"email\":\"\"," +
+            "\"password\":\"1234test\"," +
+            "\"returnSecureToken\":true" +
+            "}";
+    public String bodyLoginSpacebarInPassword ="{" +
+            "\"email\":\"test12@gmail.com\"," +
+            "\"password\":\"   \"," +
+            "\"returnSecureToken\":true" +
+            "}";
+    public String url ="https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDUR6XzgDyhoU9Ad4fw6_aoSTXEmVjuhtE";
 
 }
