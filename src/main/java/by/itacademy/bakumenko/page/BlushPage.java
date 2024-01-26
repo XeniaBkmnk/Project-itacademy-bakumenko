679bd3263c4b3d5f4dff7af159f5d4de07adc0d8
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-26 17:41:12 +0300

    edit

--- src/main/java/by/itacademy/bakumenko/page/BlushPage.java
+++ src/main/java/by/itacademy/bakumenko/page/BlushPage.java
@@ -16,37 +16,37 @@ public class BlushPage {
     }
 
     public void clickLoginInputField() {
-        driver.findElement(By.xpath(BlushPageXpath.LOGIN_INPUT_FELD)).click();
+        driver.findElement(By.xpath(BlushXpathPage.LOGIN_INPUT_FELD)).click();
     }
 
     public void clickLoginButtonMain() throws InterruptedException {
-        driver.findElement(By.xpath(BlushPageXpath.LOGIN_BUTTON_MAIN)).click();
+        driver.findElement(By.xpath(BlushXpathPage.LOGIN_BUTTON_MAIN)).click();
         Thread.sleep(1000);
     }
 
     public void sendKeysLoginInputEmail(String newEmail) throws InterruptedException {
-        driver.findElement(By.xpath(BlushPageXpath.LOGIN_INPUT_EMAIL)).sendKeys(newEmail);
+        driver.findElement(By.xpath(BlushXpathPage.LOGIN_INPUT_EMAIL)).sendKeys(newEmail);
         Thread.sleep(1000);
     }
 
     public void sendKeysInputPassword(String newPassword) {
-        driver.findElement(By.xpath(BlushPageXpath.LOGIN_INPUT_PASSWORD)).sendKeys(newPassword);
+        driver.findElement(By.xpath(BlushXpathPage.LOGIN_INPUT_PASSWORD)).sendKeys(newPassword);
     }
 
     public String getErrorMessageNonUserEmail() {
-        return driver.findElement(By.xpath(BlushPageXpath.ERROR_MESSAGE_NON_USER_EMAIL)).getText();
+        return driver.findElement(By.xpath(BlushXpathPage.ERROR_MESSAGE_NON_USER_EMAIL)).getText();
     }
 
     public String getErrorMessageNonEmail() {
-        return driver.findElement(By.xpath(BlushPageXpath.ERROR_MESSAGE_NON_EMAIL)).getText();
+        return driver.findElement(By.xpath(BlushXpathPage.ERROR_MESSAGE_NON_EMAIL)).getText();
     }
 
     public String getErrorMessageNonPassword() {
-        return driver.findElement(By.xpath(BlushPageXpath.ERROR_MESSAGE_NON_PASSWORD)).getText();
+        return driver.findElement(By.xpath(BlushXpathPage.ERROR_MESSAGE_NON_PASSWORD)).getText();
     }
 
     public String getErrorMessageNonCorrectEmail(){
-       return driver.findElement(By.xpath(BlushPageXpath.ERROR_MESSAGE_NON_CORRECT_EMAIL)).getText();
+       return driver.findElement(By.xpath(BlushXpathPage.ERROR_MESSAGE_NON_CORRECT_EMAIL)).getText();
     }
 
 }
