8163be16b98a21b5a47b2bca7c54a8d61d668b3b
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-01-27 13:10:53 +0300

    refactor LOGIN_INPUT_EMAIL

--- src/main/java/by/itacademy/bakumenko/pages/BlushXpathPage.java
+++ src/main/java/by/itacademy/bakumenko/pages/BlushXpathPage.java
@@ -4,7 +4,7 @@ public class BlushXpathPage {
     public static final String LOGIN_INPUT_FELD = "//*[@id=\"__next\"]/div[1]/div[3]/a/span";
     public static final String LOGIN_BUTTON_MAIN = "//*[@id=\"__next\"]/div[3]/div/div[1]/form/button";
     public static final String LOGIN_INPUT_PASSWORD = "//input[@id='password']";
-    public static final String LOGIN_INPUT_EMAIL = "//input[@id='email']";
+    public static final String LOGIN_INPUT_EMAIL = "//*[@id=\"email\"]";
     public static final String ERROR_MESSAGE_NON_PASSWORD = "//*[@id=\"password-error\"]";
     public static final String ERROR_MESSAGE_NON_USER_EMAIL = "//*[@id=\"__next\"]/div[3]/div/div[1]/form/div";
     public static final String ERROR_MESSAGE_NON_EMAIL = "//*[@id=\"email-error\"]";
