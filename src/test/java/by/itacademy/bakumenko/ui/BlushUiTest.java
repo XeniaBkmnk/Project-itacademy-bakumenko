8b8053fd1862ce29c1d7c5367a698b05d6f131b3
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-02-02 02:45:24 +0300

    add testSearchBar()

--- src/test/java/by/itacademy/bakumenko/ui/BlushUiTest.java
+++ src/test/java/by/itacademy/bakumenko/ui/BlushUiTest.java
@@ -12,23 +12,24 @@ public class BlushUiTest extends BaseTest {
     BlushPage blushPage = new BlushPage();
     LoginStep loginStep = new LoginStep();
     private static final Logger LOGGER = LogManager.getLogger();
+
     @Test
     public void testLoginNonUserEmail() throws InterruptedException {
-        loginStep.openLoginFormByEmailFillAndSubmit(User.getRandomCorrectEmail(),User.getRandomPassword());
-        LOGGER.info("Expected Error Message: " +"There is no user record corresponding to this identifier. The user may have been deleted.");
+        loginStep.openLoginFormByEmailFillAndSubmit(User.getRandomCorrectEmail(), User.getRandomPassword());
+        LOGGER.info("Expected Error Message: " + "There is no user record corresponding to this identifier. The user may have been deleted.");
         Assertions.assertEquals("There is no user record corresponding to this identifier. The user may have been deleted.", blushPage.getErrorMessageNonUserEmail());
     }
 
     @Test
     public void testLoginNonPassword() throws InterruptedException {
-        loginStep.openLoginFormByEmailFillAndSubmit(User.getRandomCorrectEmail(),"");
+        loginStep.openLoginFormByEmailFillAndSubmit(User.getRandomCorrectEmail(), "");
         LOGGER.info("Expected Error Message: " + "Password is required.");
         Assertions.assertEquals("Password is required.", blushPage.getErrorMessageNonPassword());
     }
 
     @Test
     public void testLoginNonCorrectEmail() throws InterruptedException {
-        loginStep.openLoginFormByEmailFillAndSubmit(User.getRandomNonCorrectEmail(),User.getRandomPassword());
+        loginStep.openLoginFormByEmailFillAndSubmit(User.getRandomNonCorrectEmail(), User.getRandomPassword());
         LOGGER.info("Expected Error Message: " + "The email address is badly formatted.");
         Assertions.assertEquals("The email address is badly formatted.", blushPage.getErrorMessageNonCorrectEmail());
     }
@@ -38,6 +39,13 @@ public class BlushUiTest extends BaseTest {
         blushPage.openMainPage();
         LOGGER.info("Expected Author Massage: " + "© Blush Design Inc. All rights reserved.");
         Assertions.assertEquals("© Blush Design Inc. All rights reserved.", blushPage.getSearchAuthorMassage());
+    }
+
+    @Test
+    public void testSearchBar() {
+        blushPage.openMainPage();
+        blushPage.sendKeysSearchBar("hobbies");
+        Assertions.assertEquals("Hobbies", blushPage.getTitleText());
     }
 
 }
